e61f4b2e58b39e83b349c041a31e37cbc894ccf7
PrinciaMalanto <mprincia.rabe@gmail.com>
2023-04-12 21:03:00 +0300

    Ajout messages erreurs et succes

--- src/main/java/mg/itu/rabe/tpbanque/jsf/transfert.java
+++ src/main/java/mg/itu/rabe/tpbanque/jsf/transfert.java
@@ -9,6 +9,7 @@ import jakarta.inject.Named;
 import jakarta.enterprise.context.RequestScoped;
 import mg.itu.rabe.tpbanque.ejb.GestionnaireCompte;
 import mg.itu.rabe.tpbanque.entities.CompteBancaire;
+import mg.itu.rabe.tpbanque.jsf.util.Util;
 
 /**
  *
@@ -58,7 +59,26 @@ public class transfert {
     public String transfertArgent() {
         CompteBancaire source = gestionnaireCompte.getCompte(this.idSource);
         CompteBancaire destination = gestionnaireCompte.getCompte(this.idDestination);
-        gestionnaireCompte.transferer(source, destination, this.montant);
+        boolean erreur = false;
+        if (source == null) {
+            // Message d'erreur associé au composant source ; form:source est l'id client
+            // si l'id du formulaire est "form" et l'id du champ de saisie de l'id de la source est "source"
+            // dans la page JSF qui lance le transfert.
+            Util.messageErreur("Aucun compte avec cet id !", "Aucun compte avec cet id !", "form:source");
+            erreur = true;
+        } else {
+            if (source.getSolde() < montant) { // à compléter pour le cas où le solde du compte source est insuffisant...
+                Util.messageErreur("Le montant est trop grand. Le solde du compte source est insuffisant.");
+                erreur = true;
+            }
+        }
+        if (erreur) { // en cas d'erreur, rester sur la même page
+            return null;
+        }
+        gestionnaireCompte.transferer(source, destination, montant);
+        // Message de succès ; addFlash à cause de la redirection.
+        Util.addFlashInfoMessage("Transfert correctement effectué");
+
         return "listeComptes?faces-redirect=true";
     }
 
